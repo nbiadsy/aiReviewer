68d1323e2e0fcdf80ccc43eed9ad72df3fce97af
Naseem Biadsy <naseem1983@gmail.com>
2025-02-21 15:44:34 +0100

    adding github and src

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello AI Reviewer");
+        randomDivision();
+    }
+
+    private static void randomDivision() {
+        Random random = new Random();
+        double x = random.nextDouble(8);
+        double y = random.nextDouble(8);
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
+        double v = x / y;
+        System.out.println("x/y: " + v);
+    }
+
+}
