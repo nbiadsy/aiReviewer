5d6d680bfe9639110a9bdc9e0ceba5cd25f62640
Naseem Biadsy <naseem1983@gmail.com>
2025-02-21 22:21:44 +0100

    add java change

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,13 @@ public class Main {
     private static void randomDivision() {
         Random random = new Random();
         double x = random.nextDouble(8);
-        double y = random.nextDouble(8);
+        double y2 = random.nextDouble(8);
 
         System.out.println("x: " + x);
-        
-        System.out.println("y: " + y);
 
-        double d = x / y;
+        System.out.println("y: " + y2);
+
+        double d = x / y2;
         System.out.println("x / y: " + d);
 
     }
